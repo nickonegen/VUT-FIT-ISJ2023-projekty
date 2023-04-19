8f28c68835679ae1e932f92874cf545abb13534a
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2023-04-20 00:30:33 +0200

    feat(proj6): finish task

--- proj6.py
+++ proj6.py
@@ -1,5 +1,115 @@
+#!/usr/bin/env python3
+
+
 class Polynomial:
-    """TODO"""
+    """Polynomial class"""
+
+    coefs: list = []  # Polynomial coefficients
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Polynomial constructor"""
+        if len(args) == 1 and isinstance(args[0], list):
+            # Argument is a list
+            self.coefs = args[0]
+        elif len(args) > 0:
+            # Argument is a tuple
+            self.coefs = list(args)
+        else:
+            # Argument is a dictionary
+            self.coefs = [0] * (max(int(k[1:]) for k in kwargs) + 1)
+            for k, v in kwargs.items():
+                self.coefs[int(k[1:])] = v
+
+        # Remove trailing zeroes
+        while len(self.coefs) > 1 and self.coefs[-1] == 0:
+            self.coefs.pop()
+
+    def __str__(self) -> str:
+        """Convert polynomial to a human-readable string"""
+        members = []  # Members of the polynomial
+
+        for i, c in reversed(list(enumerate(self.coefs))):
+            if c != 0:
+                # Sign & coefficient
+                if i < len(self.coefs) - 1:
+                    sign = "- " if c < 0 else "+ "
+                    coef = f"{abs(c)}" if abs(c) != 1 or i == 0 else ""
+                else:
+                    sign = ""
+                    coef = f"{c}" if c != 1 or i == 0 else ""
+
+                # Variable & exponent
+                if i == 1:
+                    var_exp = "x"
+                elif i > 1:
+                    var_exp = f"x^{i}"
+                else:
+                    var_exp = ""
+
+                # Combine all and append
+                members.append(f"{sign}{coef}{var_exp}")
+
+        # Combine all members and return the result
+        return " ".join(members).strip() or "0"
+
+    def __eq__(self, other) -> bool:
+        """Compare two polynomials for equality"""
+        if not isinstance(other, Polynomial):
+            return False
+
+        return self.coefs == other.coefs
+
+    def __add__(self, other) -> "Polynomial":
+        """Add two polynomials"""
+        if not isinstance(other, Polynomial):
+            return False
+
+        # Add corresponding coefficients of both polynomials
+        coefs = [a + b for a, b in zip(self.coefs, other.coefs)]
+
+        # Add remaining coefficients of the longer polynomial
+        coefs.extend(self.coefs[len(coefs) :])
+        coefs.extend(other.coefs[len(coefs) :])
+
+        return Polynomial(coefs)
+
+    def __pow__(self, n):
+        """Raise the polynomial to the power of n"""
+        result = self
+        for _ in range(n - 1):
+            result = result * self
+        return result
+
+    def __mul__(self, other):
+        """Multiply two polynomials"""
+        if not isinstance(other, Polynomial):
+            return False
+
+        # New list of coefficients with length of both polynomials
+        new_coefs = [0] * (len(self.coefs) + len(other.coefs) - 1)
+
+        # Iterate over all coefficients of both polynomials...
+        for i, a in enumerate(self.coefs):
+            for j, b in enumerate(other.coefs):
+                # ...and multiply them
+                new_coefs[i + j] += a * b
+
+        return Polynomial(new_coefs)
+
+    def derivative(self):
+        """Compute the derivative of the polynomial."""
+        # Multiply each coefficient with its exponent
+        coefs = [i * c for i, c in enumerate(self.coefs)][1:]
+        return Polynomial(coefs)
+
+    def at_value(self, x1, x2=None):
+        """Evaluate the polynomial at the given value(s) of x."""
+        if x2 is None:
+            # One arg provided -- evaluate all at x1
+            return sum(c * x1**i for i, c in enumerate(self.coefs))
+        else:
+            # Two args provided -- evaluate difference of x1 and x2
+            return self.at_value(x2) - self.at_value(x1)
 
 
 def test():
